065de3ff9890466655feb12a288b7d0632201893
Siddhant Tandon <76514713+siddhantt24@users.noreply.github.com>
2024-07-30 14:02:13 +0530

    counting the no of lines in a file.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+namespace LineCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "F:\\Sid-folder\\sid.txt"; 
+
+            try
+            {
+                int lineCount = 0;
+
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        reader.ReadLine();
+                        lineCount++;
+                    }
+                }
+
+                Console.WriteLine("Number of lines in the file: " + lineCount);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Error reading file: " + e.Message);
+            }
+        }
+    }
+}
