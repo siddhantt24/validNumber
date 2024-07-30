956a0dacf2ad695d7ab4b49fe2f500c4ab4e1a54
Siddhant Tandon <76514713+siddhantt24@users.noreply.github.com>
2024-07-30 14:08:57 +0530

    deleting a file from its location

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace FileDeletion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"F:\Sid-folder\siddhant.txt"; 
+
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    File.Delete(filePath);
+                    Console.WriteLine("File deleted successfully.");
+                }
+                else
+                {
+                    Console.WriteLine("File does not exist.");
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Error deleting file: " + e.Message);
+            }
+        }
+    }
+}
